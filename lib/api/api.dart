522cc2796db92b1626431d8906c262600a0a4f4b
Xavier Senmarti <xavier.senmarti@dribba.com>
2022-07-29 18:10:28 +0200

    Fix problems with server URL

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -26,7 +26,7 @@ class ApiSingleton {
 
   static String devBASEURL = 'https://madev.creaf.cat';
   static String prodBASEURL = 'https://webserver.mosquitoalert.com';
-  static String baseUrl = devBASEURL;
+  static String baseUrl = prodBASEURL;
   static String serverUrl = '$baseUrl/api';
 
   static String token = 'D4w29W49rMKC7L6vYQ3ua3rd6fQ12YZ6n70P';
