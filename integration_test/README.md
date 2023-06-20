8398af2ee178cb99454081bfe6ce3de758104319
Pablo Galve <pablogalve100@gmail.com>
2023-06-20 19:47:11 +0200

    starting with integration test on ios

---
+++ integration_test/README.md
@@ -0,0 +1,2 @@
+Integration tests (E2E) run on physical or virtual devices
+Used to test on multiple Android and iOS devices at once
---
+++ integration_test/dummy_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  // This dummy test will be replaced by real tests
+  testWidgets('failing test example', (tester) async {
+    expect(2 + 2, equals(4));
+  });
+}
