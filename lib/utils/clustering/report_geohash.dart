bbb3e7ea89a836bebf62df1e8c1af4070c46a2b5
Pablo Galve <pablogalve100@gmail.com>
2023-10-15 18:53:15 +0200

    remove geohash

--- lib/utils/clustering/report_geohash.dart
+++ lib/utils/clustering/report_geohash.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'package:geohash/geohash.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mosquito_alert_app/models/report.dart';
 
@@ -11,7 +10,7 @@ class ReportAndGeohash {
   String geohash;
 
   ReportAndGeohash(this.report, this.location, this.index) {
-    geohash = Geohash.encode(location.latitude, location.longitude);
+    //geohash = Geohash.encode(location.latitude, location.longitude);
   }
 
   getId() {
