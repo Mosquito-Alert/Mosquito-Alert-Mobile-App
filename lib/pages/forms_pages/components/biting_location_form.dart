6b3b902c04bf0ca7a5dd1d3ffb214837a93ea917
DigitalSeneca <digital.seneca.dev@gmail.com>
2024-01-26 10:54:59 +0100

    add return type to resetLocations

--- lib/pages/forms_pages/components/biting_location_form.dart
+++ lib/pages/forms_pages/components/biting_location_form.dart
@@ -332,7 +332,7 @@ class _BitingLocationFormState extends State<BitingLocationForm> {
     );
   }
 
-  resetLocations() {
+  void resetLocations() {
     Utils.report!.selected_location_lat = null;
     Utils.report!.selected_location_lon = null;
     Utils.report!.current_location_lat = null;
