99cb12e9aac51e83c4692828012edd01d4f6d92f
DigitalSeneca <digital.seneca.dev@gmail.com>
2024-02-01 13:57:19 +0100

    fallback to english

--- lib/utils/MyLocalizations.dart
+++ lib/utils/MyLocalizations.dart
@@ -6,10 +6,17 @@ import 'package:flutter/services.dart';
 class MyLocalizations {
   Locale locale;
   static Map<dynamic, dynamic>? _localisedValues;
+  static Map<dynamic, dynamic>? _englishValues;
 
   MyLocalizations(this.locale) {
     locale = locale;
     _localisedValues = null;
+    loadEnglishTranslations();
+  }
+
+  static Future<void> loadEnglishTranslations() async {
+    var jsonContent = await rootBundle.loadString('assets/language/en.json');
+    _englishValues = json.decode(jsonContent);
   }
 
   static Future<MyLocalizations> loadTranslations(Locale locale) async {
@@ -21,10 +28,15 @@ class MyLocalizations {
     return appTranslations;
   }
 
-  String? translate(key) {
-    return _localisedValues != null && _localisedValues![key] != null
-        ? _localisedValues![key]
-        : '';
+  String translate(key) {
+    if (_localisedValues != null && _localisedValues![key] != null){
+      return _localisedValues![key];
+    }
+    if (_englishValues != null && _englishValues![key] != null){
+      // Default to English when there is not a translation available in the local language
+      return _englishValues![key];
+    }
+    return '';
   }
 
   static String? of(BuildContext context, String? key) {
