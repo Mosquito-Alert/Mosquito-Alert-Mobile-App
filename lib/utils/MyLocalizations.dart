4539bd95f37a5d8701e5515ec12c12c88ec69958
DigitalSeneca <digital.seneca.dev@gmail.com>
2024-02-02 11:07:48 +0100

    address PR comments

--- lib/utils/MyLocalizations.dart
+++ lib/utils/MyLocalizations.dart
@@ -5,16 +5,15 @@ import 'package:flutter/services.dart';
 
 class MyLocalizations {
   Locale locale;
-  static Map<dynamic, dynamic>? _localisedValues;
-  static Map<dynamic, dynamic>? _englishValues;
+  static Map<dynamic, dynamic> _localisedValues = {};
+  static Map<dynamic, dynamic> _englishValues = {};
 
   MyLocalizations(this.locale) {
     locale = locale;
-    _localisedValues = null;
-    loadEnglishTranslations();
+    _loadEnglishTranslations();
   }
 
-  static Future<void> loadEnglishTranslations() async {
+  static Future<void> _loadEnglishTranslations() async {
     var jsonContent = await rootBundle.loadString('assets/language/en.json');
     _englishValues = json.decode(jsonContent);
   }
@@ -28,15 +27,8 @@ class MyLocalizations {
     return appTranslations;
   }
 
-  String translate(key) {
-    if (_localisedValues != null && _localisedValues![key] != null){
-      return _localisedValues![key];
-    }
-    if (_englishValues != null && _englishValues![key] != null){
-      // Default to English when there is not a translation available in the local language
-      return _englishValues![key];
-    }
-    return '';
+  String translate(String? key) {
+    return _localisedValues[key] ?? _englishValues[key] ?? '';
   }
 
   static String? of(BuildContext context, String? key) {
