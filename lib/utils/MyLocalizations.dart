b5b91dcfbd7cebaa01903bedd26ca28a3d21cebd
DigitalSeneca <digital.seneca.dev@gmail.com>
2024-05-31 14:21:57 +0200

    try to load first with region, then just language

--- lib/utils/MyLocalizations.dart
+++ lib/utils/MyLocalizations.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:path/path.dart' as path;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -21,14 +23,26 @@ class MyLocalizations {
   static Future<MyLocalizations> loadTranslations(Locale locale) async {
     var appTranslations = MyLocalizations(locale);
     String? jsonContent;
+    final regionFilename = 'assets/language/${locale.languageCode}_${locale.countryCode}.json';
+
     try {
-      jsonContent = await rootBundle.loadString(
-        'assets/language/${locale.languageCode}_${locale.countryCode}.json'
-      );
+      // Try loading the region-specific file
+      jsonContent = await rootBundle.loadString(regionFilename);
     } catch (_) {
-      jsonContent = await rootBundle.loadString(
-        'assets/language/${locale.languageCode}.json'
-      );
+      // If not found, load the language from any region
+      final supportedLanguages = ['bg_BG', 'bn_BD', 'ca_ES', 'de_DE', 'el_GR', 'en_US', 'es_ES', 'es_UY', 'eu_ES',
+        'fr_FR', 'gl_ES', 'hr_HR', 'hu_HU', 'it_IT', 'lb_LU', 'mk_MK', 'nl_NL', 'pt_PT', 'ro_RO', 'sl_SI',
+        'sq_AL', 'sr_RS', 'sv_SE', 'tr_TR',
+      ];
+
+      final code = supportedLanguages.firstWhere((lang) => lang.startsWith(locale.languageCode));
+
+      try {
+        jsonContent = await rootBundle.loadString('assets/language/' + code + '.json');
+      } catch (_) {
+        // Load the default language
+        jsonContent = await rootBundle.loadString('assets/language/en_US.json');
+      }
     }
 
     _localisedValues = json.decode(jsonContent);
