79e2583657bb38c0fbee6e44dd776ed05994e44c
DigitalSeneca <digital.seneca.dev@gmail.com>
2024-06-03 18:32:29 +0200

    Fix app not opening when region is different

--- lib/utils/MyLocalizations.dart
+++ lib/utils/MyLocalizations.dart
@@ -20,9 +20,29 @@ class MyLocalizations {
 
   static Future<MyLocalizations> loadTranslations(Locale locale) async {
     var appTranslations = MyLocalizations(locale);
-    var jsonContent = await rootBundle.loadString(
-      'assets/language/${locale.languageCode}_${locale.countryCode}.json'
-    );
+    String? jsonContent;
+    final regionFilename = 'assets/language/${locale.languageCode}_${locale.countryCode}.json';
+
+    try {
+      // Try loading the region-specific file
+      jsonContent = await rootBundle.loadString(regionFilename);
+    } catch (_) {
+      // If not found, load the language from any region
+      final supportedLanguages = ['bg_BG', 'bn_BD', 'ca_ES', 'de_DE', 'el_GR', 'en_US', 'es_ES', 'es_UY', 'eu_ES',
+        'fr_FR', 'gl_ES', 'hr_HR', 'hu_HU', 'it_IT', 'lb_LU', 'mk_MK', 'nl_NL', 'pt_PT', 'ro_RO', 'sl_SI',
+        'sq_AL', 'sr_RS', 'sv_SE', 'tr_TR',
+      ];
+
+      final code = supportedLanguages.firstWhere((lang) => lang.startsWith(locale.languageCode));
+
+      try {
+        jsonContent = await rootBundle.loadString('assets/language/' + code + '.json');
+      } catch (_) {
+        // Load the default language
+        jsonContent = await rootBundle.loadString('assets/language/en_US.json');
+      }
+    }
+
     _localisedValues = json.decode(jsonContent);
     return appTranslations;
   }
