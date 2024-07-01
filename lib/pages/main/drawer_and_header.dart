7cdcd1cfb667359a6249a9aead586abbe564f4ce
Digital Seneca <digital.seneca.dev@gmail.com>
2024-07-01 15:05:46 +0200

    Fix app freeze on first boot when trying to load from API (#172)
    * show circular progress indicator
    
    * await only for startFirstTime

--- lib/pages/main/drawer_and_header.dart
+++ lib/pages/main/drawer_and_header.dart
@@ -36,18 +36,27 @@ class _MainVCState extends State<MainVC> {
   int unreadNotifications = 0;
   var packageInfo;
   String? userUuid;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _getNotificationCount();
-    getPackageInfo();
-    initAuthStatus();
+    _startAsyncTasks();
   }
 
   @override
   void dispose() {
     super.dispose();
+  }
+
+  void _startAsyncTasks() async {
+    await UserManager.startFirstTime(context);
+    setState(() {
+      isLoading = false;
+    });
+    await _getNotificationCount();
+    await getPackageInfo();
+    await initAuthStatus();
   }
 
   Future<void> _getNotificationCount() async {
@@ -62,28 +71,25 @@ class _MainVCState extends State<MainVC> {
     });
   }
 
-  void getPackageInfo() async {
+  Future<bool> getPackageInfo() async {
     var _packageInfo = await PackageInfo.fromPlatform();
     setState(() {
       packageInfo = _packageInfo;
     });
+    return true;
   }
 
-  void initAuthStatus() async {
+  Future<bool> initAuthStatus() async {
     if (Platform.isIOS) {
       await AppTrackingTransparency.requestTrackingAuthorization();
     }
 
-    await _getData();
-  }
-
-  Future<void> _getData() async {
-    await UserManager.startFirstTime(context);
     userUuid = await UserManager.getUUID();
     UserManager.userScore = await ApiSingleton().getUserScores();
     await UserManager.setUserScores(UserManager.userScore);
     await Utils.loadFirebase();
     await Utils.getLocation(context);
+    return true;
   }
 
   late final List<Widget> _widgetOptions = <Widget>[
@@ -143,7 +149,9 @@ class _MainVCState extends State<MainVC> {
         ],
       ),
       body: Center(
-        child: _widgetOptions[_selectedIndex],
+        child: isLoading
+          ? CircularProgressIndicator()
+          : _widgetOptions[_selectedIndex],
       ),
       drawer: Drawer(
         child: Column(
