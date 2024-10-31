b138e3def7466af3b1dbfe68c869cdfd6ac4a285
Enric Pou <e.pou@ceab.csic.es>
2024-10-31 12:15:05 +0100

    Compress images before sending. (#206)
    * Compress images before sending.

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:path/path.dart' as path;
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
@@ -430,16 +433,34 @@ class ApiSingleton {
   }
 
   //Images
-  Future<bool> saveImage(String image, String? versionUUID) async {
+  Future<bool> saveImage(String imagePath, String? versionUUID) async {
     try {
-      var fileName = image.split('/').last;
-      var dio = Dio();
 
-      var img = await MultipartFile.fromFile(image,
-          filename: fileName, contentType: MediaType('image', 'jpeg'));
+      // Compressing image to jpeg 4k max = 3840x2180
+      Uint8List? compressedImage = await FlutterImageCompress.compressWithFile(
+          imagePath,
+          minWidth: 3840,
+          minHeight: 2180,
+          quality: 98,
+          autoCorrectionAngle: true,
+          format: CompressFormat.jpeg,
+          keepExif: true
+      );
+
+      if (compressedImage == null) {
+          print('Failed to compress image');
+          return false;
+      }
+
+      var img = await MultipartFile.fromBytes(
+          compressedImage,
+          filename: '${path.basenameWithoutExtension(imagePath)}.jpeg',
+          contentType: MediaType('image', 'jpeg')
+      );
 
       var data = FormData.fromMap({'photo': img, 'report': versionUUID});
 
+      var dio = Dio();
       var response = await dio.post('$serverUrl$photos',
           data: data,
           options: Options(
