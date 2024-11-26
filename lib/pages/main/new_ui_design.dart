48e7cabb29b9754e36a086a181dd870d96c1fd65
DigitalSeneca <digital.seneca.dev@gmail.com>
2024-11-26 17:12:37 +0100

    Align icons on center of row

--- lib/pages/main/new_ui_design.dart
+++ lib/pages/main/new_ui_design.dart
@@ -4,7 +4,6 @@ import 'package:mosquito_alert_app/pages/map/public_map.dart';
 import 'package:mosquito_alert_app/pages/my_reports_pages/my_reports_page.dart';
 import 'package:mosquito_alert_app/pages/settings_pages/settings_page.dart';
 
-
 class NewUIDesign extends StatefulWidget {
   @override
   _NewUIDesignState createState() => _NewUIDesignState();
@@ -32,50 +31,44 @@ class _NewUIDesignState extends State<NewUIDesign> {
       body: _pages[_selectedIndex],
       bottomNavigationBar: ClipRRect(
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30.0),
-          topRight: Radius.circular(30.0),
+          topLeft: Radius.circular(40.0),
+          topRight: Radius.circular(40.0),
         ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                offset: Offset(0, -4),
-                blurRadius: 8,
-              ),
+        child: BottomAppBar(
+          color: Colors.grey[350],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              _buildIconButton(Icons.home, 0),
+              _buildIconButton(Icons.map, 1),
+              _buildIconButton(Icons.file_copy, 2),
+              _buildIconButton(Icons.person, 3),
             ],
-          ),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home, color: Colors.black),
-                activeIcon: Icon(Icons.home, color: Colors.orange),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.map, color: Colors.black),
-                activeIcon: Icon(Icons.map, color: Colors.orange),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.file_copy, color: Colors.black),
-                activeIcon: Icon(Icons.file_copy, color: Colors.orange),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.black),
-                activeIcon: Icon(Icons.person, color: Colors.orange),
-                label: '',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            backgroundColor: Colors.grey[350],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildIconButton(IconData icon, int index) {
+    return GestureDetector(
+      onTap: () => _onItemTapped(index),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(
+            icon,
+            size: 30,
+            color: _selectedIndex == index ? Colors.orange : Colors.black,
+          ),
+          SizedBox(height: 5),
+          Container(
+            width: 30,
+            height: 2,
+            color: _selectedIndex == index ? Colors.orange : Colors.transparent,
+          )
+        ],
+      )
     );
   }
 }
