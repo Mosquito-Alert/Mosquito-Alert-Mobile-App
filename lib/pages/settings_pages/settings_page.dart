e36b7220de047288acb4e1ee40b3539c65b2c81b
DigitalSeneca <digital.seneca.dev@gmail.com>
2024-11-27 16:22:51 +0100

    Move all info to account page

--- lib/pages/settings_pages/settings_page.dart
+++ lib/pages/settings_pages/settings_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:language_picker/language_picker.dart';
 
 import 'package:language_picker/languages.dart';
+import 'package:mosquito_alert_app/pages/info_pages/info_page_webview.dart';
+import 'package:mosquito_alert_app/pages/settings_pages/campaign_tutorial_page.dart';
 import 'package:mosquito_alert_app/pages/settings_pages/components/hashtag.dart';
 import 'package:mosquito_alert_app/pages/settings_pages/components/settings_menu_widget.dart';
+import 'package:mosquito_alert_app/pages/settings_pages/partners_page.dart';
 import 'package:mosquito_alert_app/utils/Application.dart';
 import 'package:mosquito_alert_app/utils/MyLocalizations.dart';
 import 'package:mosquito_alert_app/utils/UserManager.dart';
@@ -200,7 +203,105 @@ class _SettingsPageState extends State<SettingsPage> {
                   ),
                 ],
               ),
-            )
+            ),
+            SizedBox(height: 30),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'info_scores_txt'), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => InfoPageInWebview(
+                      MyLocalizations.of(context, 'url_scoring_1'))),
+                );
+              }
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'about_the_project_txt'), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => InfoPageInWebview(
+                      MyLocalizations.of(context, 'url_about_project'))),
+                );
+              }
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'partners_txt'), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => PartnersPage()),
+              );
+            }),
+            SizedBox(height: 10),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'coordination_txt'), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => InfoPageInWebview(
+                          MyLocalizations.of(context, 'url_about_us'))),
+                );
+              }
+            ),
+            SizedBox(height: 10),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'mailing_mosquito_samples'), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CampaignTutorialPage()),
+                );
+              }
+            ),
+            SizedBox(height: 30),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'terms_of_use_txt'), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => InfoPageInWebview(
+                      MyLocalizations.of(context, 'terms_link'),
+                      localHtml: true,
+                    )
+                  ),
+                );
+              }
+            ),
+            SizedBox(height: 10),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'privacy_txt'), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => InfoPageInWebview(
+                      MyLocalizations.of(context, 'privacy_link'),
+                      localHtml: true,
+                    )
+                  ),
+              );
+              }
+            ),
+            SizedBox(height: 10),
+            SettingsMenuWidget(
+              MyLocalizations.of(context, 'license_txt'), () {
+                Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => InfoPageInWebview(
+                    MyLocalizations.of(context, 'lisence_link'),
+                    localHtml: true,
+                  )
+                ),
+              );
+              }
+            ),
           ]),
         ),
       ),
