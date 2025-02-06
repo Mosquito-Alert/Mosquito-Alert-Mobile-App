ff3845d89cc2cb7ad56d76ae5281dfbe50a04259
DigitalSeneca <digital.seneca.dev@gmail.com>
2025-02-06 10:05:52 +0100

    Upgrade android minSdk to 23

--- android/app/src/main/kotlin/com/example/mosquito_alert_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mosquito_alert_app/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.mosquito_alert_app
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import dev.fluttercommunity.workmanager.WorkmanagerPlugin
 
 class MainActivity: FlutterActivity() {
-
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(WorkmanagerPlugin())
+    }
 }
