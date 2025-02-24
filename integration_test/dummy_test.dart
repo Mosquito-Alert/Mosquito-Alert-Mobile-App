6a94a2e8c54e7ad914e01eb4836ebe20385a8b47
DigitalSeneca <digital.seneca.dev@gmail.com>
2025-02-24 10:44:48 +0100

    reorder to have iOS emulator start at beginning

--- integration_test/dummy_test.dart
+++ integration_test/dummy_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mosquito_alert_app/main.dart' as app;
