4d2c69880787360cbf30bdffd4361346684bd346
DigitalSeneca <digital.seneca.dev@gmail.com>
2025-02-24 13:02:41 +0100

    Showing app instead of "Test starting..."

--- integration_test/dummy_test.dart
+++ integration_test/dummy_test.dart
@@ -1,19 +1,20 @@
 //import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mosquito_alert_app/main.dart' as app;
 
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
 
   group('end-to-end test', () {
-    testWidgets('Sanity test', (
-      tester,
-    ) async {
+    testWidgets('Sanity test', (tester) async {
       app.main(env: "dev");
-      await tester.pumpAndSettle();
+      await tester.pumpAndSettle(Duration(seconds: 3));
 
+      // Expect notification permission being asked
       expect(true, isTrue);
       //expect(find.byKey(ValueKey("testing")), findsOne);
     });
