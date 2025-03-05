13e171a36859e0dbdd340f841c00dca4893c91f5
Enric Pou <enricpou@gmail.com>
2025-03-05 13:44:40 +0100

    Removed duplicate workmanager registration message

--- android/app/src/main/kotlin/com/example/mosquito_alert_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mosquito_alert_app/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.example.mosquito_alert_app
 
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import dev.fluttercommunity.workmanager.WorkmanagerPlugin
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        flutterEngine.plugins.add(WorkmanagerPlugin())
-    }
 }
