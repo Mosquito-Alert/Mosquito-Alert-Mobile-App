f6b323d86cc6ae8b6bbc8fcb75bbedf3708050b2
DigitalSeneca <digital.seneca.dev@gmail.com>
2025-03-28 09:44:47 +0100

    Remove unused import

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -8,7 +8,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:in_app_review/in_app_review.dart';
 import 'package:mosquito_alert_app/api/api.dart';
