5574503c802051f6157305a4c00db74f6895a113
Digital Seneca <digital.seneca.dev@gmail.com>
2025-06-30 15:27:01 +0200

    Fixes using new api (#385)
    * Fixes using new api
    
    * init FixApi in api.dart
    
    * return bool depending on response statusCode

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -25,6 +25,7 @@ class ApiSingleton {
 
   static late MosquitoAlert api;
   static late AuthApi authApi;
+  static late FixesApi fixesApi;
 
   static final ApiSingleton _singleton = ApiSingleton._internal();
 
@@ -63,6 +64,7 @@ class ApiSingleton {
     );
 
     authApi = api.getAuthApi();
+    fixesApi = api.getFixesApi();
 
     // Try to restore session if we have stored credentials
     final apiUser = await UserManager.getApiUser();
@@ -240,12 +242,6 @@ class ApiSingleton {
       return false;
     }
 
-    // TODO
-    return false;
-  }
-
-  Future<bool> sendFixes(String trackingUuid, double lat, double lon,
-      DateTime time, int power) async {
     // TODO
     return false;
   }
