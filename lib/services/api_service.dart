f43fe677da1f656fa7cd3361fb8196463c2f8392
Digital Seneca <digital.seneca.dev@gmail.com>
2025-09-15 18:01:13 +0200

    Fix 'JwtAuthInterceptor' isn't defined for the type 'ApiService'

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:mosquito_alert/mosquito_alert.dart';
-import 'package:mosquito_alert/src/auth/jwt_auth.dart';
 import 'package:mosquito_alert_app/app_config.dart';
 import 'package:mosquito_alert_app/providers/auth_provider.dart';
 
@@ -18,15 +17,40 @@ class ApiService {
     );
     final Dio _dio = Dio(options);
 
-    _dio.interceptors.add(JwtAuthInterceptor(
-        options: options,
-        getAccessToken: () async => authProvider.accessToken ?? '',
-        getRefreshToken: () async => authProvider.refreshToken ?? '',
-        onTokenUpdateCallback: (newAccessToken) {
-          authProvider.setAccessToken(accessToken: newAccessToken);
-        }));
+    _dio.interceptors.add(LogInterceptor(
+      requestBody: false,
+      responseBody: false,
+      requestHeader: false,
+      responseHeader: false,
+      request: true,
+      error: true,
+    ));
 
-    _client = MosquitoAlert(dio: _dio);
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final token = authProvider.accessToken;
+          if (token != null && token.isNotEmpty) {
+            options.headers['Authorization'] = 'Bearer $token';
+          } else {
+            print(
+                'Auth interceptor: No token available for ${options.method} ${options.path}');
+          }
+          handler.next(options);
+        },
+        onError: (error, handler) async {
+          if (error.response?.statusCode == 401) {
+            print('Auth interceptor: 401 error, token might be expired');
+          }
+          handler.next(error);
+        },
+      ),
+    );
+
+    _client = MosquitoAlert(
+      dio: _dio,
+      basePathOverride: baseUrl,
+    );
   }
 
   static Future<ApiService> init({required AuthProvider authProvider}) async {
