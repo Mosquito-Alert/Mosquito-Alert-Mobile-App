2ce35ce73c231d7e82f87c81ac62134b5e0d239a
Digital Seneca <digital.seneca.dev@gmail.com>
2025-10-23 08:52:44 +0200

    remove unused import

--- lib/pages/reports/shared/pages/notes_and_submit_page.dart
+++ lib/pages/reports/shared/pages/notes_and_submit_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:mosquito_alert_app/utils/MyLocalizations.dart';
 import 'package:mosquito_alert_app/utils/style.dart';
