c0118eea7cb01590f7897ee1e4559ed01e4075c4
Enric Pou <enricpou@gmail.com>
2025-11-07 16:44:15 +0100

    Show card when is an active campaign

--- lib/pages/my_reports_pages/detail/adult_report_detail_page.dart
+++ lib/pages/my_reports_pages/detail/adult_report_detail_page.dart
@@ -1,9 +1,13 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:mosquito_alert/mosquito_alert.dart';
 import 'package:mosquito_alert_app/pages/my_reports_pages/detail/shared_report_widgets.dart';
 import 'package:mosquito_alert_app/pages/my_reports_pages/widgets/report_detail_page.dart';
+import 'package:mosquito_alert_app/pages/settings_pages/campaign_tutorial_page.dart';
+import 'package:mosquito_alert_app/utils/MyLocalizations.dart';
 import 'package:mosquito_alert_app/utils/report_formatter.dart';
+import 'package:mosquito_alert_app/utils/style.dart';
 import 'package:provider/provider.dart';
 
 class AdultReportDetailPage extends StatefulWidget {
@@ -20,6 +24,7 @@ class AdultReportDetailPage extends StatefulWidget {
 
 class _AdultReportDetailPageState extends State<AdultReportDetailPage> {
   late ObservationsApi observationsApi;
+  Campaign? activeCampaign;
 
   @override
   void initState() {
@@ -35,10 +40,27 @@ class _AdultReportDetailPageState extends State<AdultReportDetailPage> {
     );
   }
 
-  void _initializeApi() {
+  void _initializeApi() async {
     MosquitoAlert apiClient =
         Provider.of<MosquitoAlert>(context, listen: false);
     observationsApi = apiClient.getObservationsApi();
+
+    final observationCountry = widget.observation.location.country;
+    final observationDateAllowsCampaign =
+        DateTime.now().difference(widget.observation.createdAt) <=
+            Duration(days: 2);
+    if (observationCountry != null && observationDateAllowsCampaign) {
+      final _campaignsApi = apiClient.getCampaignsApi();
+      final campaignsResponse = await _campaignsApi.list(
+        countryId: observationCountry.id,
+        isActive: true,
+        pageSize: 1,
+        orderBy: ['-start_date'].build(),
+      );
+      setState(() {
+        activeCampaign = campaignsResponse.data?.results?.firstOrNull;
+      });
+    }
   }
 
   Future<void> _deleteReport({required Observation observation}) async {
@@ -76,6 +98,74 @@ class _AdultReportDetailPageState extends State<AdultReportDetailPage> {
       extraListTileMap: extraListTileMap,
       topBarBackgroundBuilder: (observation) =>
           ReportDetailWidgets.buildPhotoCarousel(report: observation),
+      cardBuilder: () => activeCampaign != null
+          ? Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text.rich(
+                            TextSpan(
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: MyLocalizations.of(context,
+                                          "alert_campaing_found_title") +
+                                      " ",
+                                ),
+                                TextSpan(
+                                  text: widget.observation.shortId,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            softWrap: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      activeCampaign!.postingAddress,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey[600],
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CampaignTutorialPage()),
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                          foregroundColor: Style.colorPrimary,
+                        ),
+                        child: Text(
+                          MyLocalizations.of(context, "show_info"),
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : const SizedBox.shrink(),
     );
   }
 }
