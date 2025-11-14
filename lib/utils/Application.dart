6891d865551e7ecbe869c846974a345d6cbbb953
Enric Pou <enricpou@gmail.com>
2025-11-14 11:58:14 +0100

    Fix timeago translations

--- lib/utils/Application.dart
+++ lib/utils/Application.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:timeago/timeago.dart' as timeago;
+
 class Application {
   static final Application _application = Application._internal();
 
@@ -7,10 +9,60 @@ class Application {
     return _application;
   }
 
-  Application._internal();
+  Application._internal() {
+    for (var entry in _localeMessages.entries) {
+      timeago.setLocaleMessages(entry.key, entry.value);
+    }
+  }
+
+  final Map<String, timeago.LookupMessages> _localeMessages = {
+    // 'bg',
+    'bn': timeago.BnMessages(),
+    'bn_short': timeago.BnShortMessages(),
+    'ca': timeago.CaMessages(),
+    'ca_short': timeago.CaShortMessages(),
+    'de': timeago.DeMessages(),
+    'de_short': timeago.DeShortMessages(),
+    'el': timeago.GrMessages(),
+    'el_short': timeago.GrShortMessages(),
+    'en': timeago.EnMessages(),
+    'en_short': timeago.EnShortMessages(),
+    'es': timeago.EsMessages(),
+    'es_short': timeago.EsShortMessages(),
+    'fr': timeago.FrMessages(),
+    'fr_short': timeago.FrShortMessages(),
+    'hr': timeago.HrMessages(),
+    'hr_short': timeago.HrMessages(),
+    'hu': timeago.HuMessages(),
+    'hu_short': timeago.HuShortMessages(),
+    'it': timeago.ItMessages(),
+    'it_short': timeago.ItShortMessages(),
+    'lb': timeago.DeMessages(),
+    'lb_short': timeago.DeShortMessages(),
+    // 'mk': ,
+    'nl': timeago.NlMessages(),
+    'nl_short': timeago.NlShortMessages(),
+    'pt': timeago.PtBrMessages(),
+    'pt_short': timeago.PtBrShortMessages(),
+    'ro': timeago.RoMessages(),
+    'ro_short': timeago.RoShortMessages(),
+    'ru': timeago.RuMessages(),
+    'ru_short': timeago.RuShortMessages(),
+    // 'sl': ,
+    // 'sq': ,
+    'sr': timeago.SrMessages(),
+    'sr_short': timeago.SrShortMessages(),
+    'sv': timeago.SvMessages(),
+    'sv_short': timeago.SvShortMessages(),
+    'tr': timeago.TrMessages(),
+    'tr_short': timeago.TrShortMessages(),
+    'zh': timeago.ZhCnMessages(),
+    'zh_short': timeago.ZhCnMessages(),
+  };
 
   final List<String> supportedLanguagesCodes = [
     'bg',
+    'bn',
     'ca',
     'de',
     'el',
@@ -29,6 +81,7 @@ class Application {
     'sl',
     'sq',
     'sr',
+    'sv',
     'tr',
     'zh',
   ];
